9db2f861c1512bcecd4a391fc333b2ddc1097714
Juan Luis Martínez Rocha <jl.canlo@yahoo.com.mx>
2021-02-28 20:19:59 -0600

    Carga inicial de APP: Incluye swipeCards llamada API de películas y títulos

---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+class PeliculasProvider {
+  String _apikey = 'bb9afef0aa37b1012bfc24903d2a255d';
+  String _url = 'api.themoviedb.org';
+  String _languaje = 'es-ES';
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+
+    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+
+    return peliculas.items;
+  }
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing',
+        {'api_key': _apikey, 'language': _languaje});
+
+    return await _procesarRespuesta(url);
+  }
+
+  Future<List<Pelicula>> getPopulares() async {
+    final url = Uri.https(
+        _url, '3/movie/popular', {'api_key': _apikey, 'language': _languaje});
+
+    return await _procesarRespuesta(url);
+  }
+}
